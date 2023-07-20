cfa706291889c081148d1b58f8faa4a26cdc742a
Parker Parham <parkerparham@gmail.com>
2023-07-20 15:19:49 -0400

    Adds content to Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,10 @@
+import { Container, Heading, Text } from "@chakra-ui/react";
+
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <Container>
+      <Heading my="30px" p="10px">Chakra UI Components</Heading>
+      <Text ml="30px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, eos?</Text>
+    </Container>
   )
 }
