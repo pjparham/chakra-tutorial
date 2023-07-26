96633b439be65cd2a69ac51a9ae130461e91495a
Parker Parham <parkerparham@gmail.com>
2023-07-26 14:42:36 -0400

    Updates navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,24 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, HStack, Heading, Spacer, Text  } from "@chakra-ui/react";
 
 export default function Navbar() {
   return (
-    <Flex bg="gray.200" justify="space-between" wrap="wrap" gap="2">
-        <Box w="150px" h="50px" bg="red">1</Box>
-        <Box w="150px" h="50px" bg="blue">2</Box>
-        <Box w="150px" h="50px" flexGrow="1" bg="green">3</Box>
-        <Box w="150px" h="50px" flexGrow="2" bg="yellow">4</Box>
+    <Flex as="nav" p="10px" alignItems="center">
+        <Heading as="h1">Dojo Tasks</Heading>
+        <Spacer />
+
+        <HStack spacing="20px">
+            <Box bg="gray.200" p="10px">M</Box>
+            <Text>parker@netninja.dev</Text>
+            <Button colorScheme="purple">Logout</Button>
+        </HStack>
     </Flex>
+
+
+    // <Flex bg="gray.200" justify="space-between" wrap="wrap" gap="2">
+    //     <Box w="150px" h="50px" bg="red">1</Box>
+    //     <Box w="150px" h="50px" bg="blue">2</Box>
+    //     <Box w="150px" h="50px" flexGrow="1" bg="green">3</Box>
+    //     <Box w="150px" h="50px" flexGrow="2" bg="yellow">4</Box>
+    // </Flex>
   )
 }
