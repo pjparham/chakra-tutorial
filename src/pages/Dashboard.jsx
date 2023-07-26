f4c5488d4484bf26f14b8d23b29403aa9370438e
Parker Parham <parkerparham@gmail.com>
2023-07-26 16:37:20 -0400

    Adds sidebar and updates dashboard content

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,34 +1,25 @@
-import { Box, Container, Heading, Text } from "@chakra-ui/react";
+import { Box, SimpleGrid } from "@chakra-ui/react";
 
 export default function Dashboard() {
 
-  const boxStyles = {
-    p: "10px",
-    bg: "purple.400",
-    color:  "white",
-    m: "10px",
-    textAlign: "center",
-    filter: 'blur(2px)',
-    ':hover': {
-      color: "black",
-      bg: "blue.200",
-      filter: 'none'
-    }
-  }
+
 
   return (
-    <Container as="section" maxW="4xl" py="20px">
-      <Heading my="30px" p="10px">Chakra UI Components</Heading>
-      <Text ml="30px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, eos?</Text>
-      <Text marginLeft="30px" color="blue.300" fontWeight="bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, eos?</Text>
+    <SimpleGrid columns={4} spacing={10} minChildWidth="200px" p="10px">
+      <Box bg="white"  h="200px" border="1px solid"></Box>
+      <Box bg="white"  h="200px" border="1px solid"></Box>
+      <Box bg="white"  h="200px" border="1px solid"></Box>
+      <Box bg="white"  h="200px" border="1px solid"></Box>
 
-      <Box my="30px" p="20px" bg="orange">
-        <Text color="white">This is a box</Text>
-      </Box>
+      <Box bg="white"  h="200px" border="1px solid"></Box>
+      <Box bg="white"  h="200px" border="1px solid"></Box>
+      <Box bg="white"  h="200px" border="1px solid"></Box>
+      <Box bg="white"  h="200px" border="1px solid"></Box>
 
-      <Box sx={boxStyles}>
-        Hello, Everyone!
-      </Box>
-    </Container>
+      <Box bg="white"  h="200px" border="1px solid"></Box>
+      <Box bg="white"  h="200px" border="1px solid"></Box>
+      <Box bg="white"  h="200px" border="1px solid"></Box>
+      <Box bg="white"  h="200px" border="1px solid"></Box>
+    </SimpleGrid>
   )
 }
