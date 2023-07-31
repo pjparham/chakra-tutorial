5ead26d85864cf0747e7e060ab20ec93ef5328ce
Parker Parham <parkerparham@gmail.com>
2023-07-31 15:39:25 -0400

    Adds toast component for logout response

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,20 @@
-import { Box, Button, Flex, HStack, Heading, Spacer, Text  } from "@chakra-ui/react";
+import { Box, Button, Flex, HStack, Heading, Spacer, Text, useToast  } from "@chakra-ui/react";
 
 export default function Navbar() {
+  const toast = useToast()
+
+  const showToast = () => {
+    toast({
+      title: 'Logged out',
+      description: 'Successfully logged out',
+      duration: 5000,
+      isClosable: true,
+      status: 'success',
+      position: 'top', 
+      variant: 'subtle'
+    })
+  }
+
   return (
     <Flex as="nav" p="10px" alignItems="center" mb="40px">
         <Heading as="h1">Dojo Tasks</Heading>
@@ -9,7 +23,7 @@ export default function Navbar() {
         <HStack spacing="20px">
             <Box bg="gray.200" p="10px">M</Box>
             <Text>parker@netninja.dev</Text>
-            <Button colorScheme="purple">Logout</Button>
+            <Button colorScheme="purple" onClick={showToast}>Logout</Button>
         </HStack>
     </Flex>
   )
